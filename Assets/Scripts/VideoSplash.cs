a765fb05a1ac2d8d000a8b829e73ee25fe261d49
Frondelan <ser0012011@hotmail.com>
2019-04-14 14:44:15 -0600

    Splash listo
    - Finalización del splash screen

---
+++ Assets/Scripts/VideoSplash.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoSplash : MonoBehaviour
+{
+    public VideoPlayer videoSplash;
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoSplash.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
