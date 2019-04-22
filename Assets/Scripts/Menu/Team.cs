a1ee8b073c4e99be39d479fd32f18967ca877244
Frondelan <ser0012011@hotmail.com>
2019-04-21 22:01:09 -0600

    Desarrolladores 0.1

---
+++ Assets/Scripts/Menu/Team.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Team : MonoBehaviour
+{
+    public GameObject panelTeam;
+
+    public void OpenPanel()
+    {
+        if (panelTeam != null)
+        {
+            bool isActive = panelTeam.activeSelf;
+            panelTeam.SetActive(!isActive);
+        }
+    }
+}
