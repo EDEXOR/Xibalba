87885db9c3d455f40247c7c32a6b134c59ebd73d
Frondelan <ser0012011@hotmail.com>
2019-04-28 14:29:01 -0600

    Museo 0.1

---
+++ Assets/Resources/Museo/Siguiente.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Siguiente : MonoBehaviour
+{
+    public Camera camPerrito;
+    public Camera camPiramide;
+
+    void start()
+    {
+        camPerrito.enabled = true;
+        camPiramide.enabled = false;
+        print("INICIANDO");
+    }
+
+    public void verPiramide()
+    {
+        camPerrito.enabled = false;
+        camPiramide.enabled = true;
+        print("PIRAMIDE");
+    }
+
+    public void verPerrito()
+    {
+        camPerrito.enabled = true;
+        camPiramide.enabled = false;
+        print("PERRITO");
+    }
+    
+}
