434c08d326f47eb9ed8ab6ae2c48fb9b3ff5544c
Frondelan <ser0012011@hotmail.com>
2019-04-27 18:33:21 -0600

    ScreenShot 0.1
    Función de ss en vista de Cihuatán

---
+++ Assets/Scripts/ScreenShot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class ScreenShot : MonoBehaviour
+{
+    public void TakeShot()
+    {
+        StartCoroutine("CaptureIt");
+    }
+
+    IEnumerator CaptureIt()
+    {
+        yield return new WaitForEndOfFrame();
+        string data = System.DateTime.Now.ToFileTime().ToString();
+        string fileName = "Cihutan" + data + ".png";
+        string pathToSave = fileName;
+        ScreenCapture.CaptureScreenshot(pathToSave);
+
+        //string origin = System.IO.Path.Combine(Application.persistentDataPath, fileName);
+        //string destination = "/internalstorage/ScreenCapture/" + fileName; // could be anything 
+
+
+    }
+}
