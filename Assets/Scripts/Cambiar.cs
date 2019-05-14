fba5f9a4fa2ae3aacf2162d29ef053e30960873d
Frondelan <ser0012011@hotmail.com>
2019-05-14 16:05:11 -0600

    Museo 0.2

--- Assets/Scripts/Cambiar.cs
+++ Assets/Scripts/Cambiar.cs
@@ -2,12 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Cambiar : MonoBehaviour
 {
+    public Slider sliderRotacion;
+    public GameObject objeto3D;
     
     public void cambiarEscena(string nombre)
     {
         SceneManager.LoadScene(nombre);
+    }
+
+    public void rotarObjeto()
+    {
+        objeto3D.transform.rotation = Quaternion.Euler(0, sliderRotacion.value, 0);
     }
 }
